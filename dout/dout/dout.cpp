8039955948160815a5a1f07bf2138bb92bea0746
sergiy revkov <sergiy.revkov@gmail.com>
2016-08-08 10:31:50 -0400

    initial commit

---
+++ dout/dout/dout.cpp
@@ -0,0 +1,21 @@
+// dout.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+
+LPCTSTR g_default_message = TEXT("dout default message");
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	const TCHAR* msg = g_default_message;
+	if (argc > 1)
+	{
+		msg = argv[1];
+	}
+
+	OutputDebugString(msg);
+
+	return 0;
+}
+
