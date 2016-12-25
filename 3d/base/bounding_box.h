b17c746dd5ff283c418111f384d25ce79a566d64
sergiy revkov <sergiy.revkov@gmail.com>
2016-12-25 09:25:23 -0500

    simple bounding box

---
+++ 3d/base/bounding_box.h
@@ -0,0 +1,18 @@
+#ifndef __bounding_box_
+#define __bounding_box_
+
+#include "vertex.h"
+
+namespace base
+{
+  class bounding_box
+  {
+  public:
+    bounding_box() {}
+  private:
+    base::vertex min_;
+    base::vertex max_;
+  };
+}
+
+#endif //__bounding_box_
