3c57ef8cc4f722251a3a549dd584449bfd909f8c
sergiy revkov <sergiy.revkov@gmail.com>
2016-12-25 09:26:15 -0500

    simple test console

---
+++ 3d/base/test.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include "vertex.h"
+#include "bounding_box.h"
+
+int main(int argc, char* argv[])
+{
+  printf("[m]test console (c) 2016\n");
+  printf("[m]start tests\n");
+
+  base::vertex v;
+  base::bounding_box bb;
+  
+  printf("[m]exit\n");
+  return 0;
+}
+ 
